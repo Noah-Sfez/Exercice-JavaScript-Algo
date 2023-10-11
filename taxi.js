7ee53da945461b224244ffa3f2c333bf5e7fb0f9
Noah Sfez <sfz.noah@gmail.com>
2023-10-11 11:58:12 +0200

    :tada: Exercice Taxi FINI !

---
+++ taxi.js
@@ -0,0 +1,28 @@
+let Personnage = {
+    name: 'John',
+    health: 10,
+}
+
+let musiques = ['Anissa - Wejdene', 'Billie jean - Michael Jackson', 'Talking to the Moon - Bruno mars', 'de la merde - Wejdene', 'pas ouf - Wejdene']
+
+
+for(let feux = 30; feux>=0; feux--  ){
+    let musiquealea = musiques[Math.floor(Math.random()*5)]
+    console.log(musiquealea + " passe à la radio...")
+    if(musiquealea === "Anissa - Wejdene"){
+        console.log("éteins moi cette musique de merde !")
+        Personnage.health --
+        console.log("Sa santé mentale est à " + Personnage.health)
+    }
+    console.log(feux + " feux")
+    if(feux == 0 && Personnage.health > 0){
+        let changements = 10 - Personnage.health
+        console.log("Il lui aura fallu " + changements + " changements pour arriver à destination")
+    }else if(Personnage.health ==0){
+        console.log("Explosion")
+        break
+    } else {
+        continue
+    }
+    
+}
